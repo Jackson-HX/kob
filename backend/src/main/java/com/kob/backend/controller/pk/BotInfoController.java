0679d4b81f69cdab3e36bfd13f437ddc3f57bb7c
HXXJKS <hangxuan@umich.edu>
2024-08-18 20:58:31 -0400

    project structure constructed

--- backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
+++ backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
@@ -4,12 +4,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/pk/")
 public class BotInfoController {
 
     @RequestMapping("getbotinfo/")
-    public String getBotInfo (){
-        return "returned string";
+    public Map<String, String> getBotInfo (){
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("name","booby");
+        map.put("rating","100");
+        return map;
     }
 }
