27842b32b6bb3bf98586367103584af847160828
HXXJKS <hangxuan@umich.edu>
2024-08-31 01:01:16 -0400

    6.1 finished weird map sync bug

--- backend/src/main/java/com/kob/backend/consumer/WebSocketServer.java
+++ backend/src/main/java/com/kob/backend/consumer/WebSocketServer.java
@@ -2,6 +2,7 @@ package com.kob.backend.consumer;
 
 import com.alibaba.fastjson2.JSONObject;
 import com.kob.backend.Mapper.UserMapper;
+import com.kob.backend.consumer.utils.Game;
 import com.kob.backend.consumer.utils.JwtAuthentication;
 import com.kob.backend.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,6 +28,7 @@ public class WebSocketServer {
     private Session session = null;
 
     private static UserMapper userMapper;
+    private Game game = null;
 
     @Autowired
     public void setUserMapper(UserMapper userMapper) {
@@ -71,16 +73,21 @@ public class WebSocketServer {
             matchpool.remove(a);
             matchpool.remove(b);
 
+            Game game = new Game(13, 14, 20);
+            game.createMap();
+
             JSONObject respA = new JSONObject();
             respA.put("event", "start-matching");
             respA.put("opponent_username", b.getUsername());
             respA.put("opponent_photo", b.getPhoto());
+            respA.put("gamemap", game.getG());
             users.get(a.getId()).sendMessage(respA.toJSONString());
 
             JSONObject respB = new JSONObject();
             respB.put("event", "start-matching");
             respB.put("opponent_username", a.getUsername());
             respB.put("opponent_photo", a.getPhoto());
+            respB.put("gamemap", game.getG());
             users.get(b.getId()).sendMessage(respB.toJSONString());
 
         }
--- web/src/assets/scripts/GameMap.js
+++ web/src/assets/scripts/GameMap.js
@@ -3,11 +3,12 @@ import { GridBlock } from "./GridBlock";
 import { Snake } from "./Snake";
 
 export class GameMap extends GameObject {
-  constructor(ctx, parent) {
+  constructor(ctx, parent, store) {
     super();
 
     this.ctx = ctx;
     this.parent = parent;
+    this.store = store;
     this.L = 0;
 
     this.rows = 13;
@@ -22,60 +23,8 @@ export class GameMap extends GameObject {
     ];
   }
 
-  is_connected(g, sx, sy, tx, ty) {
-    if (sx == tx && sy == ty) return true;
-    g[sx][sy] = true;
-    let dx = [-1, 0, 1, 0],
-      dy = [0, 1, 0, -1];
-    for (let i = 0; i < 4; i++) {
-      let new_x = sx + dx[i];
-      let new_y = sy + dy[i];
-      if (!g[new_x][new_y] && this.is_connected(g, new_x, new_y, tx, ty)) {
-        return true;
-      }
-    }
-    return false;
-  }
-
   create_blocks() {
-    const g = [];
-
-    // init all grids
-    for (let r = 0; r < this.rows; r++) {
-      g[r] = [];
-      for (let c = 0; c < this.cols; c++) {
-        g[r][c] = false;
-      }
-    }
-
-    // add blocks to surrounding grids
-    for (let r = 0; r < this.rows; r++) {
-      g[r][0] = g[r][this.cols - 1] = true;
-    }
-    for (let r = 0; r < this.cols; r++) {
-      g[0][r] = g[this.rows - 1][r] = true;
-    }
-
-    // randomly generated inner blocks
-    for (let i = 0; i < this.inner_blocks_cnt / 2; i++) {
-      for (let j = 0; j < 1000; j++) {
-        let r = parseInt(Math.random() * this.rows);
-        let c = parseInt(Math.random() * this.cols);
-        if (
-          g[r][c] ||
-          g[this.rows - 1 - r][this.cols - 1 - c] ||
-          (r == this.rows - 2 && c == 1) ||
-          (r == 1 && c == this.cols - 2)
-        )
-          continue;
-        g[r][c] = g[this.rows - 1 - r][this.cols - 1 - c] = true;
-        break;
-      }
-    }
-
-    const copy_g = JSON.parse(JSON.stringify(g));
-    if (!this.is_connected(copy_g, this.rows - 2, 1, 1, this.cols - 2))
-      return false;
+    const g = this.store.state.pk.gamemap;
 
     for (let r = 0; r < this.rows; r++) {
       for (let c = 0; c < this.cols; c++) {
@@ -84,8 +33,6 @@ export class GameMap extends GameObject {
         }
       }
     }
-
-    return true;
   }
 
   add_listening_events() {
@@ -105,11 +52,8 @@ export class GameMap extends GameObject {
   }
 
   start() {
-    for (let i = 0; i < 1000; i++) {
-      if (this.create_blocks()) {
-        break;
-      }
-    }
+    this.create_blocks();
+
     this.add_listening_events();
   }
 
